22cc9fcafe4547bc2d11b9dc076b20085bf0d9f7
MR14Titan14 <sansen184@gmail.com>
2024-02-16 16:21:22 +0300

    Акинатор правки

--- Akinator.pl
+++ Akinator.pl
@@ -33,11 +33,10 @@ question(drama):-
 	
 question(detective):-
     query('You wished a detective story?').
+
+question(oscar):-
+    query('Did your film winned oscar?').
 	
-geg:-
-bagof(X,asked(X,y),L),
-length(L,A),
-write(A).
 	
 film(doomsday):-
     question(thriller),
@@ -45,27 +44,27 @@ film(doomsday):-
 	question(science_fiction),
     bagof(X,asked(X,y),L),
     length(L,A),
-    A=3.
+    A=3,!.
 
 film(night_hunter):-
 	question(thriller),
     question(detective),
     bagof(X,asked(X,y),L),
     length(L,A),
-    A=2.
+    A=2,!.
 
 film(drive):-
 	question(drama),
     question(thriller),
     bagof(X,asked(X,y),L),
     length(L,A),
-    A=2.
+    A=2,!.
 
 film(fight_club):-
     question(thriller),
     bagof(X,asked(X,y),L),
     length(L,A),
-    A=1.
+    A=1,!.
 
 film(coraline):-
 	question(horror),
@@ -73,48 +72,48 @@ film(coraline):-
 	question(science_fiction),
     bagof(X,asked(X,y),L),
     length(L,A),
-    A=3.
+    A=3,!.
 
 film(hell_boy):-
 	question(horror),
     question(cartoon),
     bagof(X,asked(X,y),L),
     length(L,A),
-    A=2.
+    A=2,!.
 
 film(alien_vs_predator):-
 	question(horror),
     question(science_fiction),
     bagof(X,asked(X,y),L),
     length(L,A),
-    A=2.
+    A=2,!.
 
 film(scary_movie):-
 	question(horror),
     question(comedy),
     bagof(X,asked(X,y),L),
     length(L,A),
-    A=2.
+    A=2,!.
 
 film(astral):-
     question(horror),
     bagof(X,asked(X,y),L),
     length(L,A),
-    A=1.
+    A=1,!.
 
 film(the_gentlemen):-
 	question(action_movie),
     question(comedy),
     bagof(X,asked(X,y),L),
     length(L,A),
-    A=2.
+    A=2,!.
 		
 film(murder_on_the_orient_express):-
 	question(drama),
     question(detective),
     bagof(X,asked(X,y),L),
     length(L,A),
-    A=2.
+    A=2,!.
 	
 film(soul):-
 	question(cartoon),
@@ -128,51 +127,58 @@ film(elemental):-
     question(drama),
     bagof(X,asked(X,y),L),
     length(L,A),
-    A=2.
+    A=2,!.
 	
 film(war_of_the_worlds):-
     question(action_movie),
 	question(science_fiction),
     bagof(X,asked(X,y),L),
     length(L,A),
-    A=2.
+    A=2,!.
 	
 film(knives_out):-
     question(comedy),
     question(detective),
     bagof(X,asked(X,y),L),
     length(L,A),
-    A=2.
+    A=2,!.
 	
+film(one_plus_one):-
+    question(comedy),
+    question(oscar),
+    bagof(X,asked(X,y),L),
+    length(L,A),
+    A=1,!.
+
 film(drujit_po_russki):-
     question(comedy),
     bagof(X,asked(X,y),L),
     length(L,A),
-    A=1.
+    A=1,!.
 	
 film(shrek):-
     question(cartoon),
     bagof(X,asked(X,y),L),
     length(L,A),
-    A=1.
+    A=1,!.
 	
 film(interstellar):-
     question(science_fiction),
     bagof(X,asked(X,y),L),
     length(L,A),
-    A=1.
+    A=1,!.
 	
 film(operation_fortune):-
     question(action_movie),
     bagof(X,asked(X,y),L),
     length(L,A),
-    A=1.
+    A=1,!.
 	
 film(the_green_mile):-
     question(drama),
     bagof(X,asked(X,y),L),
     length(L,A),
-    A=1.
+    A=1,!.
 
 
 
