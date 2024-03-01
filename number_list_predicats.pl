209690966eb50568c1f23b90d34f2b077277f807
MR14Titan14 <sansen184@gmail.com>
2024-03-01 17:43:32 +0300

    Изменения в комментариях к предикатам из задания 1

--- number_list_predicats.pl
+++ number_list_predicats.pl
@@ -20,6 +20,7 @@ fact2(N,A,X):- N>0,
     X is X1.
 call_fact2(N,X):-fact2(N,1,X).
 
+%cifr_sum(+N,-S)
 cifr_sum(0,0):-!.
 cifr_sum(N,S):-N1 is N div 10,cifr_sum(N1,S1),S is S1 + N mod 10.
 
@@ -28,7 +29,7 @@ cifr_sum_down(N,S):-cifr_sum_down(N,S,0).
 cifr_sum_down(0,S,S):-!.
 cifr_sum_down(N,S,X):-X1 is X + N mod 10,N1 is N div 10,cifr_sum_down(N1,S,X1).
 
-
+%call_square_free(+Number)
 call_square_free(Number):-square_free(Number,2).
 
 square_free(Number,Number):-!.
@@ -40,14 +41,16 @@ Moded \= 0,
 Squarer_temp is Squarer + 1,
 square_free(Number,Squarer_temp).
 
+%read_list(+N,-List)
 read_list(0,[]):-!.
 read_list(N,[Head|Tail]) :- read(Head), NewN is N - 1,
     read_list(NewN,Tail).
 
+%write_list(+List)
 write_list([]) :- !.
 write_list([H|T]) :- write(H), nl, write_list(T).
 
-
+%sum_list_down(+List,-Sum)
 sum_list_down(List,Sum) :- sum_list_down(List,0,Sum).
 sum_list_down([],CurSum,CurSum):-!.
 sum_list_down([H|T],CurSum,Sum) :- NewSum is CurSum + H,
@@ -59,6 +62,7 @@ read_sum_write(Count):- read_list(Count,List),
 sum_list_down(List,Sum),
 write(Sum).
 
+%sum_list_up(+List,-Sum)
 sum_list_up([],0):-!.
 sum_list_up([H|T],Sum) :- sum_list_up(T,SumTail), Sum is SumTail + H.
 
