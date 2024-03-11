c1da045d2e6482bed85e7e4c65eaedec544021b0
MR14Titan14 <sansen184@gmai.com>
2024-03-11 15:35:12 +0300

    2.9 добавлен предикат count_min

--- num_arrays.pl
+++ num_arrays.pl
@@ -8,9 +8,19 @@ write_list([]) :- !.
 write_list([Head|Tail]) :- write(Head), nl, write_list(Tail).
 
 %count(+List,+Element,-Count)
+%Подсчитывает количество вхождений элемента Element в список List, количество вхождений сохраняет в Count
 count(List, Element, Count):-
   findall(Element, member(Element, List), Counters),
   length(Counters, Count).
+
+%slice(+List,+From,+To,-Result)
+%Возвращает список элементов между элементами с индексами From и To списка List
+slice(List, From, To, Result):-
+  Fromt is From + 1, Tot is To + 1,
+  length(LFrom, Fromt),
+  length([_|LTo], Tot),
+  append(LTo, _, List),
+  append(LFrom, Result, LTo).
 
 %is_glob_max(+Index,+List)
 is_glob_max():-
@@ -24,3 +34,16 @@ max_list(List,Max),
 Number =:= Max,
 count(List,Number,Count),
 Count =:= 1.
+
+%count_min(+List,+A,+B,-Count)
+count_min(Count):-
+write('Enter number of elements: '),
+read(N),
+read_list(N,List),
+write('Enter index A: '),
+read(A),
+write('Enter index B: '),
+read(B),
+min_list(List,Min),
+slice(List,A,B,Sublist),
+count(Sublist,Min,Count).
